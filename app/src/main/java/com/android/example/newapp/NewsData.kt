7f30ef5fc5dd3d2caad56d9b161290cca8cb10c0
AniekanJames <jamesanny20@gmail.com>
2021-10-29 14:47:46 +0100

    Detail view update.

--- app/src/main/java/com/android/example/newapp/NewsData.kt
+++ app/src/main/java/com/android/example/newapp/NewsData.kt
@@ -15,15 +15,15 @@ data class NewsData(
 @Parcelize
 data class Article(
     val source: Source,
-    val author: String,
+    val author: String?,
     val title: String,
-    val description: String,
+    val description: String?,
     val url: String,
-    val urlToImage: String,
-    val publishedAt: String,
-    val content: String): Parcelable
+    val urlToImage: String?,
+    val publishedAt: String?,
+    val content: String?): Parcelable
 
 @Parcelize
 data class Source(
-    val id: String,
+    val id: String?,
     val name: String): Parcelable
--- app/src/main/java/com/android/example/newapp/NewsDetailFragment.kt
+++ app/src/main/java/com/android/example/newapp/NewsDetailFragment.kt
@@ -11,7 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.android.example.newapp.Article
 import kotlinx.android.synthetic.main.fragment_news_details.*
 
-class NewsDetailFragment : Fragment() {
+class NewsDetailFragment : Fragment(R.layout.fragment_news_details) {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -25,7 +25,7 @@ class NewsDetailFragment : Fragment() {
 
         detailView.apply { 
             webViewClient = WebViewClient()
-            loadUrl(article.url)
+             loadUrl(article.url)
         }
 
 
